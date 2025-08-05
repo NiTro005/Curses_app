2ec2430c5d9886fdd71eecce1c9c1f7180503423
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-08-06 00:15:07 +0300

    Added display of all courses in the grid

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,14 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
+import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +50,83 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CoursesApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CoursesApp() {
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues().calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues().calculateEndPadding(layoutDirection)
+            )
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .background(Color(0xFF4A148C))
+            )
+            CourseList(
+                topicList = DataSource.topics,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun CourseList(topicList: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        modifier = modifier.padding(8.dp),
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(topicList) { topic ->
+            CourseCart(topic)
+        }
+    }
+}
+
+@Composable
+fun CourseCart(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.height(68.dp)) {
+        Row {
+            Image(
+                painter = painterResource(topic.image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxHeight().width(68.dp).padding(end = 16.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column {
+                Text(
+                    text = stringResource(topic.string),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(top = 16.dp, end = 16.dp)
+                )
+                Row(modifier = Modifier.padding(top = 8.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(R.drawable.school_24px),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = topic.count.toString(),
+                        modifier = Modifier.padding(start = 8.dp),
+                        style = MaterialTheme.typography.labelMedium
                     )
                 }
             }
@@ -30,18 +134,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoursesTheme {
-        Greeting("Android")
-    }
+fun CourseCartPreview() {
+    CourseCart(Topic(R.string.architecture, 58, R.drawable.architecture))
 }
